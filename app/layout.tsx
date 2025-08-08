a13367226229fc8c9422a60622d84e60281d6963
Jorge Calleja <posicionadoenlaweb@gmail.com>
2025-08-09 00:22:05 +0200

    feat: vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { GeistSans } from "geist/font/sans";
 import { GeistMono } from "geist/font/mono";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next";
 
 export const metadata: Metadata = {
   title: "Retro-GameHub - Juegos Clásicos en Línea",
